492e4940b58c0ad806439fdfff045111a8aeb36f
yjie10 <yy.jie28@gmail.com>
2021-11-22 17:16:14 -0800

    homepage layout basic styling done

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,53 +1,46 @@
 import React from 'react';
 
+import './homepage.styles.scss'
+
 const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
+
       <div className="menu-item">
         <div className="content">
           <h1 className="title">PLUSH</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
 
-    <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
           <h1 className="title">FIGURES & PINS</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
 
-    <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
           <h1 className="title">GAME</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
 
-    <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
           <h1 className="title">APPAREL</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
 
-    <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
           <h1 className="title">ACCESSORIES</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
 
-    <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
           <h1 className="title">STATIONARIES</h1>

c82d20c8aaea92217fe69717cfafa28b155d3d1b
yjie10 <yy.jie28@gmail.com>
2021-11-25 17:26:54 -0800

    more notes + withRouter with MenuItem (for access to history) and added onclick to navigate to corresponding category

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl }) => (
-  <div className='menu-item'>
+const MenuItem = ({ title, imageUrl, history }) => (
+  <div className='menu-item' onClick={() => history.push(`/${title.replace(/\s/g, '')}`)}>
     <div className='background-image' style={{
       backgroundImage: `url(${imageUrl})`
     }} />
@@ -15,4 +16,4 @@ const MenuItem = ({ title, imageUrl }) => (
   </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);

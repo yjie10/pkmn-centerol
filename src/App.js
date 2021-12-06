42a9d32f8aa6b3d9dd3b34d6d6e07c35c843085f
yjie10 <yy.jie28@gmail.com>
2021-12-06 14:04:36 -0800

    created signin component and signinsignup-page component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import SignInSignUpPage from './pages/signin-signup-page/signin-signup-page.component';
 
 import Header from './components/header/header.component';
 
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );

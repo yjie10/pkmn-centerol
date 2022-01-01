00c5312e506b9d0705eaf6eed096bf6d6601acbb
yjie10 <yy.jie28@gmail.com>
2022-01-01 10:43:46 -0800

    new year commit :) summary of what was done 11.22.21 - 12.06.21

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import './form-input.styles.scss';
 
-
 const FormInput = ({ handleChange, label, ...otherProps }) => (
   <div className='group'>
     <input className='form-input' onChange={handleChange} {...otherProps} />

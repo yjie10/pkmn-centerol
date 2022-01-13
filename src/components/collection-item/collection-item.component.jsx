9a6f9702662ef5e17baef58aa078d0b2e902a033
yjie10 <yy.jie28@gmail.com>
2022-01-13 14:29:21 -0800

    custom-button to have inverted and added 'add to cart' when hovered on collection items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, imageUrl, price }) => (
@@ -13,6 +15,7 @@ const CollectionItem = ({ id, name, imageUrl, price }) => (
       <span className='price'>{price}</span>
 
     </div>
+    <CustomButton inverted>ADD TO CART</CustomButton>
   </div>
 );
 

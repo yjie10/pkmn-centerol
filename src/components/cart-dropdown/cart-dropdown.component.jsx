5d8eb92d28d980177916670d76fc568c47759131
yjie10 <yy.jie28@gmail.com>
2022-01-18 17:38:07 -0800

    dispatch shorthand -- hide cart (toggleCartHidden) when going to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,14 +3,16 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import { createStructuredSelector } from 'reselect';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
@@ -19,7 +21,10 @@ const CartDropdown = ({ cartItems, history }) => (
           <span className='empty-message'>Your cart is empty. </span>
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>CHECKOUT</CustomButton>
+    <CustomButton onClick={() => {
+      history.push('/checkout');
+      dispatch(toggleCartHidden());
+    }}>CHECKOUT</CustomButton>
   </div>
 );
 

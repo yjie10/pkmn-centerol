223884b2b638a4cee7fcc7b1c94441acca738106
yjie10 <yy.jie28@gmail.com>
2022-01-23 15:52:52 -0800

    collection overview component (moving .map into its own component)

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import { createStructuredSelector } from 'reselect';
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
-
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
   <div className='shop-page'>
-    {
-      collections.map(({ id, ...otherCollectionProps }) => <CollectionPreview key={id} {...otherCollectionProps} />)
-    }
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;

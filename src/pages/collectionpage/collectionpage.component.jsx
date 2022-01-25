ce0bb7d95b6dd596b198b2da2992da1cf3862a82
yjie10 <yy.jie28@gmail.com>
2022-01-25 14:11:45 -0800

    finishing up collection page to display the correct category of items

--- src/pages/collectionpage/collectionpage.component.jsx
+++ src/pages/collectionpage/collectionpage.component.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-// import collectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectShopCollection } from '../../redux/shop/shop.selectors';
 
+import './collectionpage.styles.scss';
+
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
   return (
-    <div className='category'>
-      <h2>CATEGORY PAGE</h2>
+    <div className='collection-page'>
+      <h2>{title}</h2>
+      <div className='items'>
+        {
+          items.map(item => <CollectionItem key={item.id} item={item} />)
+        }
+      </div>
     </div>)
 }
 
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,38 @@ const INITIAL_STATE = {
   categories: [{
     title: 'plush',
     imageUrl: 'https://lh3.googleusercontent.com/pw/AM-JKLXPt7nMnc-CxvuV8LSRmbQMs_l7UU9lm1WBWfFyDmvvYhGj1GJ5_ZoUOdKQiQbhHGhLfRMNwmdQ1LS8I2PaA_EiZeBUhF-EyDJacsbJLjN3YXhQUNvRIeutX-k6wsCy4eez57owTmbNe-Svm0JkILvs=w1306-h956-no?authuser=0',
-    id: 1
+    id: 1,
+    linkUrl: 'shop/plush'
   },
   {
     title: 'figures & pins',
     imageUrl: 'https://www.pokemoncenter.com/site/binaries/content/gallery/bloomreach/responsive-banners/2021/11-november-2021/202111_banner_holidaypins_desktop.jpg',
-    id: 2
+    id: 2,
+    linkUrl: 'shop/figures&pins'
   },
   {
     title: 'game',
     imageUrl: 'https://lh3.googleusercontent.com/pw/AM-JKLXb4uB0PJhtidazz0c901ISx6-BPurKU5r4Xt47Pz5oGJFMcSgo_J1BjtKlKbv1nH7q7rVvCdTUdBhfCqFRWD7J-9P_Q5TWJYBAtltGh68Y73_riVhAtuV9aHnNVHcZVdzQBJOIqqCA6B3FEIQ53LX3=w1698-h724-no?authuser=0',
-    id: 3
+    id: 3,
+    linkUrl: 'shop/game'
   },
   {
     title: 'apparel',
     imageUrl: 'https://lh3.googleusercontent.com/pw/AM-JKLWh2TVf_slWZKgiNgxkonm5QRVToCoHklHKg56_48C3VPVOvHe36jblxQgYIympY59ESTVu_COysFGmXLSl9kzdEgMH8HiLO8Rxa-MBj5VBAkRsXgqf9Eqco-ZapQzwZtc-1P5jqCWZq-SAhTbxuaVf=w1168-h963-no?authuser=0',
-    id: 4
+    id: 4,
+    linkUrl: 'shop/apparel'
   },
   {
     title: 'accessories',
     imageUrl: 'https://lh3.googleusercontent.com/pw/AM-JKLVi6zqOzwPqiG1JipgQLQTvnHWLoB9k8CCbKIHm2iG4-9rAjI-S0O91-51iYOitjxe-Mwgo9Z1uzuAZPFf_P8gjgtNBVmuN0M5r5kKPhZiYszJY0z65-5_OkrxT1lnDWndEnRI9FiT_I9RmMgWtt0BZ=w980-h792-no?authuser=0',
-    id: 5
+    id: 5,
+    linkUrl: 'shop/accessories'
   },
   {
     title: 'stationery',
     imageUrl: 'https://lh3.googleusercontent.com/pw/AM-JKLUyzbbPdauQ01sZMGfxbulBRBNn-yVR6WNo64WyQMuED5RA2mYiKaffQh_-QJ7NOdtKFat14iPotPpUzonUwLl4LHKfwPkJBRCEet4ryfTUFXdh9DuCbZeGHshDlP_LvItWT7FDBZmcZamSjsvr4b0x=w1282-h608-no?authuser=0',
-    id: 6
+    id: 6,
+    linkUrl: 'shop/stationery'
   }
   ]
 };

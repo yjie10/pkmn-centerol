395e1524ce7264b5b692e1f42bdf6043f97eb6f4
yjie10 <yy.jie28@gmail.com>
2022-02-12 14:04:20 -0800

    removed shop data on the frontend and build the with-spinner component to wrap components when it is in the loading state

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './with-spinner.styles.scss';
+
+const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
+  return isLoading ? (
+    <div className='spinner-overlay'>
+      <div className='spinner' />
+    </div>
+  ) : <WrappedComponent {...otherProps} />
+}
+
+export default WithSpinner;

f5ddf34cb4a61351db54f27de90b9089c57825a2
yjie10 <yy.jie28@gmail.com>
2022-02-11 19:11:55 -0800

    changed shop page into class component and fetching data from firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -38,6 +38,25 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 }
 
+export const convertCollctionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  }
+  )
+  // console.log(transformedCollection);
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {})
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
